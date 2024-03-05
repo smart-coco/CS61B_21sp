97749d895764be4ed55ce8a873ddce51d2cabc4e
smart-coco <1044357360@qq.com>
2024-03-06 00:42:22 +0800

    proj1_修正remove触发shrink的条件

--- proj1/src/test/java/deque/MaxArrayDeque.java
+++ proj1/src/test/java/deque/MaxArrayDeque.java
@@ -1,5 +1,13 @@
 package deque;
 
-public class MaxArrayDeque {
-    
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    public MaxArrayDeque(Comparator<T> c) {
+
+    }
+    public T max() {
+        if (size()==0) return null;
+        else {
+            return 1;
+        }
+    }
 }
