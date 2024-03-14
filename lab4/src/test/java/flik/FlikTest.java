f9f26bb752db3aeccdce61cac3d0005c05f85487
smart-coco <1044357360@qq.com>
2024-03-15 01:42:40 +0800

    lab4_第一次提交

---
+++ lab4/src/test/java/flik/FlikTest.java
@@ -0,0 +1,11 @@
+package flik;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FlikTest {
+   @Test 
+   public void randomTest() {
+    assertTrue("Not equals", Flik.isSameNumber(130,130)); 
+   }
+}
